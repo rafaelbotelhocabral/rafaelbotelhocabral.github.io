72edd897527cc4a8f925ba7927e8e1ef3d243e52
Rafael Botelho Cabral <110874514+rafaelcabral23415@users.noreply.github.com>
2022-12-30 17:01:35 -0300

    Trabalho final

--- sitefinal/sitefinal.js
+++ sitefinal/sitefinal.js
@@ -1,48 +1,44 @@
 class MobileNavbar {
-
-  constructor(mobileMenu) {
+    constructor(mobileMenu, navList, navLinks) {
       this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector();
+      this.navList = document.querySelector(navList);
       this.navLinks = document.querySelectorAll(navLinks);
       this.activeClass = "active";
-
+  
       this.handleClick = this.handleClick.bind(this);
-  }
-
-  animateLinks(){
+    }
+  
+    animateLinks() {
       this.navLinks.forEach((link, index) => {
-          link.style.animation
-              ? (link.style.animation = "")
-              : (link.style.animation = `navLinkFade 0.5s ease forwards $
-              {index / 7 + 0.3s}s`);
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
       });
-  }
-
-  addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-  }
-
-  handleClick() {
+    }
+  
+    handleClick() {
       this.navList.classList.toggle(this.activeClass);
       this.mobileMenu.classList.toggle(this.activeClass);
       this.animateLinks();
-
-  }
-
-  Init() {
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
       if (this.mobileMenu) {
-          this.addClickEvent();
+        this.addClickEvent();
       }
       return this;
+    }
   }
-}
-
-const MobileNavbar = new MobileNavbar(
-".mobile-menu",
-".nav-list",
-".nav-list li",
-
-);
-MobileNavbar.Init();
-
-
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
